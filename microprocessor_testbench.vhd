985ec75f17863a321363655f5e0b517e3db74498
pratonian <pratb369@gmail.com>
2016-11-01 10:45:59 +0530

    testbench for microprocessor

---
+++ microprocessor_testbench.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+--use ieee.std_logic_unsigned.all;
+use STD.textio.all;
+--use ieee.std_logic_textio.all;
+use ieee.numeric_std.all;
+
+entity microprocessors is
+
+port ()
+
+end entity;
+
+
